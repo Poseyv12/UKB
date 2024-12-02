d5043159da8c6cedee874636a05a53962735ae58
Vance Poesy <vancepoesy@Vances-MBP.attlocal.net>
2024-12-02 15:31:11 -0600

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
-
+import { SpeedInsights } from '@vercel/speed-insights/next';
 export const metadata: Metadata = {
   title: 'Ultimate Kitchen & Bath',
   description: 'Premier kitchen and bathroom remodeling services',
@@ -15,6 +15,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
+        <SpeedInsights />
       </body>
     </html>
   )
