4b815fac037b7f551315d9891ebf74ab35d73b8d
Vance Poesy <poseyv12@gmail.com>
2025-04-02 22:39:06 -0500

    upfdates

--- lib/sanity.client.ts
+++ lib/sanity.client.ts
@@ -2,12 +2,19 @@ import { createClient } from 'next-sanity';
 import imageUrlBuilder from '@sanity/image-url';
 import { SanityImageSource } from '@sanity/image-url/lib/types/types';
 
+if (!process.env.NEXT_PUBLIC_SANITY_PROJECT_ID) {
+  throw new Error('Missing NEXT_PUBLIC_SANITY_PROJECT_ID environment variable');
+}
+
+if (!process.env.NEXT_PUBLIC_SANITY_DATASET) {
+  throw new Error('Missing NEXT_PUBLIC_SANITY_DATASET environment variable');
+}
+
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: '2024-01-01',
   useCdn: process.env.NODE_ENV === 'production',
-  token: process.env.SANITY_API_TOKEN,
   perspective: 'published',
 });
 
